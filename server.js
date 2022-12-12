a00ff03b17cf01b2ab7fd4fddbd0d11cf9a40061
Michael Nunez <michael@Michaels-Laptop.local>
2022-12-12 16:02:00 -0600

    syntax errors

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express()
 
 app.get('/', () => {
   console.log('They are at our server!')
-}
+})
 
 app.get('/testpath', () => {
   console.log('The test path has worked!')
-}
+})
 
 app.listen(80)
