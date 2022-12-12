6bf656c57a0914e41635a3b90caf4bf3436e2c56
Michael Nunez <michael@Michaels-Laptop.local>
2022-12-12 15:56:18 -0600

    init

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.get('/', () => {
+  console.log('They are at our server!')
+}
+
+app.get('/testpath', () => {
+  console.log('The test path has worked!')
+}
+
+app.listen(80)
